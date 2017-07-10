fa093cfaf1041642a48a895c44795d6ffba35715
Marcel Hodan <marcel.hodan@gmail.com>
2017-07-10 09:34:00 +0200

    create a jetty embedded server

--- jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/Static.java
+++ jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/Static.java
@@ -1,4 +1,11 @@
 package de.marcelhodan.blog.jettyembedded.httpserver;
 
+import org.eclipse.jetty.server.Server;
+
 public class Static {
+	public static void main(String[] args) throws Exception {
+		// Create Server
+		Server server = new Server(8080);
+
+	}
 }
