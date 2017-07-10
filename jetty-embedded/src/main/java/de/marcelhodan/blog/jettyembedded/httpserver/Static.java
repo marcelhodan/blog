60f1938633659b1d36f6e48b53a97c8a3f1cea00
Marcel Hodan <marcel.hodan@gmail.com>
2017-07-10 09:35:18 +0200

    tell the server where to find the base directory

--- jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/Static.java
+++ jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/Static.java
@@ -1,11 +1,17 @@
 package de.marcelhodan.blog.jettyembedded.httpserver;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 public class Static {
 	public static void main(String[] args) throws Exception {
 		// Create Server
 		Server server = new Server(8080);
+
+		// Configure ResourceHandler
+		ResourceHandler resource_handler = new ResourceHandler();
+		resource_handler.setDirectoriesListed(true);
+		resource_handler.setResourceBase(".");
 
 	}
 }
