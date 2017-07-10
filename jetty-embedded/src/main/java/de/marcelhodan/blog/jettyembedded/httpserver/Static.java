5bcd0097e795f32f0b0492c7637b44ab32858f54
Marcel Hodan <marcel.hodan@gmail.com>
2017-07-10 09:36:10 +0200

    add the resource handler to the server.

--- jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/Static.java
+++ jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/Static.java
@@ -1,6 +1,9 @@
 package de.marcelhodan.blog.jettyembedded.httpserver;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 
 public class Static {
@@ -12,6 +15,11 @@ public class Static {
 		ResourceHandler resource_handler = new ResourceHandler();
 		resource_handler.setDirectoriesListed(true);
 		resource_handler.setResourceBase(".");
+
+		// Add the ResourceHandler to the server.
+		HandlerList handlers = new HandlerList();
+		handlers.setHandlers(new Handler[] { resource_handler });
+		server.setHandler(handlers);
 
 	}
 }
