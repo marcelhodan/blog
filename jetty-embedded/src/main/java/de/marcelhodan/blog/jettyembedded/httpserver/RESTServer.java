3d7bb56453412562e299919f0f93a8f4c665f633
Marcel Hodan <marcel.hodan@gmail.com>
2017-07-12 18:45:43 +0200

    create ServletContainer and add it to the ContextHandler

--- jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/RESTServer.java
+++ jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/RESTServer.java
@@ -2,15 +2,20 @@ package de.marcelhodan.blog.jettyembedded.httpserver;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class RESTServer {
 	public static void main(String[] args) throws Exception {
 		// Create Server
-		Server server = new Server(8080);
+		Server server = new Server(8090);
 
 		// Configure ServletContextHandler
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
 		server.setHandler(context);
+
+		// Create Servlet Container
+		ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+		jerseyServlet.setInitOrder(0);
 	}
 }
