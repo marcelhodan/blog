1e63cc7c024a53c7dc401124667b30fe848b38ec
Marcel Hodan <marcel.hodan@gmail.com>
2017-07-12 18:32:50 +0200

    create a jetty embedded server

--- jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/RESTServer.java
+++ jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/RESTServer.java
@@ -1,5 +1,10 @@
 package de.marcelhodan.blog.jettyembedded.httpserver;
 
-public class RESTServer {
+import org.eclipse.jetty.server.Server;
 
+public class RESTServer {
+	public static void main(String[] args) throws Exception {
+		// Create Server
+		Server server = new Server(8080);
+	}
 }
