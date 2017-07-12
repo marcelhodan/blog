bea9396bb309688c46f07fddd6cadcfa426d591f
Marcel Hodan <marcel.hodan@gmail.com>
2017-07-12 18:38:02 +0200

    add a new handler for ServletContext with contextPath to the server

--- jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/RESTServer.java
+++ jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/RESTServer.java
@@ -1,10 +1,16 @@
 package de.marcelhodan.blog.jettyembedded.httpserver;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 
 public class RESTServer {
 	public static void main(String[] args) throws Exception {
 		// Create Server
 		Server server = new Server(8080);
+
+		// Configure ServletContextHandler
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
 	}
 }
