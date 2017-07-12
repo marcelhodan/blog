1cc89689692c9b9c9912dc8cd4c93fac4df7ca7e
Marcel Hodan <marcel.hodan@gmail.com>
2017-07-12 18:50:28 +0200

    create DummyClass and define initParameter of the servlet

--- jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/RESTServer.java
+++ jetty-embedded/src/main/java/de/marcelhodan/blog/jettyembedded/httpserver/RESTServer.java
@@ -1,10 +1,25 @@
 package de.marcelhodan.blog.jettyembedded.httpserver;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class RESTServer {
+	@Path("/dummy")
+	public class DummyClass {
+		@GET
+		@Path("test")
+		@Produces(MediaType.TEXT_PLAIN)
+		public String test() {
+			return "my test";
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 		// Create Server
 		Server server = new Server(8090);
@@ -17,5 +32,12 @@ public class RESTServer {
 		// Create Servlet Container
 		ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
 		jerseyServlet.setInitOrder(0);
+
+		// Tells the Jersey Servlet which REST service/class to load.
+		jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", DummyClass.class.getCanonicalName());
+
+		// Start the server
+		server.start();
+		server.join();
 	}
 }
